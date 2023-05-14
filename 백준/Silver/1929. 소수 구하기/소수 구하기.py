a29b2f051b55dc4a241dc7cd50e5d9817f8b4fdf
kkimjehyeon <65211547+dlsrks0631@users.noreply.github.com>
2023-05-14 23:52:03 +0900

    [Silver III] Title: 소수 구하기, Time: 192 ms, Memory: 122948 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+import sys
+
+m, n = map(int, sys.stdin.readline().split())
+
+is_prime = [True] * (n + 1)
+is_prime[0] = is_prime[1] = False
+
+
+for i in range(2, int(n**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i * 2, n + 1, i):
+            is_prime[j] = False
+
+for i in range(m, n + 1):
+    if is_prime[i]:
+        print(i)
