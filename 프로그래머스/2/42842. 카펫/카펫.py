b813d1784344f7f700029e4d8ecfa46db715c4bc
kkimjehyeon <65211547+dlsrks0631@users.noreply.github.com>
2024-03-14 17:37:06 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,19 +1,3 @@
 def solution(brown, yellow):
-    total = brown + yellow
-
-    lst = getDivision(total)
-    print(lst)
-
-    for height, width in lst:
-        if (height - 2) * (width - 2) >= yellow:
-            return ([width, height])
-
-
-def getDivision(number):
-    result = []
-
-    for i in range(1, int(number ** 1/2) + 1):
-        if number % i == 0:
-            result.append((i, number // i))
-    
-    return result[0:len(result)//2 + 1]
+    answer = []
+    return answer
