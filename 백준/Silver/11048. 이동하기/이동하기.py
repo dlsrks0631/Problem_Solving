de6c4f262289739a9bc07b6949bc5c7eae04e7d0
kkimjehyeon <65211547+dlsrks0631@users.noreply.github.com>
2024-07-09 12:35:06 +0900

    [Silver II] Title: 이동하기, Time: 2396 ms, Memory: 78944 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,23 +1,29 @@
 import sys
+sys.setrecursionlimit(1000000)
 
-input = sys.stdin.readline
+def func(i, j):
+    global arr, dp
 
-# 3, 4
-N,M = map(int,input().split())
-dp = []
+    # base case
+    if dp[i][j] != -1:
+        return dp[i][j]
 
-for _ in range(N):
-    dp.append(list(map(int,input().split())))
+    # recursive case
+    dp[i][j] = max(func(i - 1, j), func(i, j - 1), func(i - 1, j - 1)) + arr[i][j]
+    return dp[i][j]
 
-# 초기값 세팅
-for i in range(1, N):
-    dp[i][0] = dp[i-1][0] + dp[i][0]
 
-for j in range(1, M):
-    dp[0][j] = dp[0][j-1] + dp[0][j]
+N, M = map(int, input().split())
 
-for i in range(1, N):
-    for j in range(1, M):
-        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + dp[i][j]
+arr = [[0] + list(map(int, input().split())) for _ in range(N)]
+arr = [[0] * (M + 1)] + arr
 
-print(dp[N-1][M-1])
+dp = [[-1 for _ in range(M + 1)] for _ in range(N + 1)]
+
+for j in range(0, M + 1):
+    dp[0][j] = 0
+
+for i in range(0, N + 1):
+    dp[i][0] = 0
+
+print(func(N,M))
